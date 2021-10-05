8fae8076fe6d9a41256eb33b33693c239b3fcf46
delihiros <delihiros@gmail.com>
2021-10-06 01:21:16 +0900

    fix: fix failing unmarshal

--- pkg/entities/current_season.go
+++ pkg/entities/current_season.go
@@ -1,7 +1,5 @@
 package entities
 
-import "encoding/json"
-
 type CurrentSeason struct {
 	Currenttier          int    `json:"currenttier"`
 	Currenttierpatched   string `json:"currenttierpatched"`
@@ -9,10 +7,4 @@ type CurrentSeason struct {
 	MmrChangeToLastGame  int    `json:"mmr_change_to_last_game"`
 	Elo                  int    `json:"elo"`
 	GamesNeededForRating int    `json:"games_needed_for_rating"`
-}
-
-// TODO: incorrect implementation
-func (s *CurrentSeason) UnmarshalJSON(data []byte) error {
-	var m map[string]*json.RawMessage
-	return json.Unmarshal(data, &m)
 }
